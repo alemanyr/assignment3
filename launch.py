88d04794b49a5a17dac8848719046308db4fd1ad
alemanyr <ralemany@uci.edu>
2020-11-07 18:09:14 -0800

    basic stuff

--- launch.py
+++ launch.py
@@ -7,18 +7,15 @@ def main(inputFilePath):
 	zipFile = inputFilePath
 	print("zip file location is: " + zipFile)
 	
-	with zipfile.ZipFile(zipFile, "r") as f:
-		for filename in f.namelist():
-			if(filename.endswith('.json')):
-				# print(ntpath.basename(filename))	
-				# TODO tokenizer class/functions 
-				tokenize(zipFile, ntpath.basename(filename))
-				time.sleep(300)
+	with zipfile.ZipFile(zipFile, "r") as zipped:
+		for filename in zipped.namelist():
+			if (filename.endswith('.json')):
+				with zipped.open(filename) as f:
+					content = json.loads(f.read())
+					#print(content)
+					tokenize(content)
+					time.sleep(300)
 				
-	
-		
-	
-	
 if __name__ == "__main__":
 	main(sys.argv[1])
 
--- tokenizer.py
+++ tokenizer.py
@@ -1,12 +1,19 @@
-import json
+import json, time, re, nltk, requests
 from bs4 import BeautifulSoup
-import zipfile
+from simhash import Simhash
+nltk.download('punkt')
 
-
-def tokenize(zipfilepath, filename):
-	website = zipfile.ZipFile(zipfilepath, 'r')
-		
-	data = json.loads(website.read(filename))
-		
-	print("\n" +data+ "\n")
+def tokenize(content):
+	#print(content["content"])
+	chars = set('@.,/:;()-_') #set of chars that dont count as tokens on their own
+	
+	soup = BeautifulSoup(content["content"], 'html.parser')
+	text_tokens = nltk.tokenize.word_tokenize(soup.get_text())
+	for s in text_tokens:
+		if not any((c in chars) for c in s):
+			print(s)
+	time.sleep(300)
+	# TODO: do stuff with text_tokens and write inverse index to content_file
+	#with open("content.txt", 'a', encoding = "utf-8") as content_file:
+	#	print("hi")	
 	
