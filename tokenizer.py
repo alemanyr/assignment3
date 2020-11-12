ea3acffa5a32d177dd434fa46a75f805d2198529
alemanyr <ralemany@uci.edu>
2020-11-11 21:03:04 -0800

    fixed duplicate keys/postings

--- tokenizer.py
+++ tokenizer.py
@@ -12,15 +12,18 @@ class Tokenizer:
 	
 		soup = BeautifulSoup(content["content"], 'html.parser')
 		text_tokens = nltk.tokenize.word_tokenize(soup.get_text())
-		#TODO: convert all tokens to lowercase and remove duplicates
-		
-		for s in text_tokens:
+		tokens2 = []
+		for item in text_tokens:
+			item = item.lower()
+			if item not in tokens2:
+				tokens2.append(item)
+		for s in tokens2:
 			if not any((c in chars) for c in s): #
 				if s not in self.inverted:
 					self.inverted[s] = [docID]
-				if s in self.inverted: #TODO: dont append to key if docID value is already posted
+				elif (s, docID) not in self.inverted.items(): #TODO: dont append to key if docID value is already posted
 					self.inverted[s].append(docID)
-					print(s + " not in the inverted list\n")
+					#print(s + " not in the inverted list\n")
 		print(self.inverted)			
 		#time.sleep(300)
 			
