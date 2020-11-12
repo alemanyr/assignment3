c04913e9ab57ee0fa61aa2282dba192caae1d93c
alemanyr <ralemany@uci.edu>
2020-11-11 18:20:02 -0800

    tokenizer class progress

--- launch.py
+++ launch.py
@@ -1,19 +1,21 @@
 import zipfile, sys, getopt, time, json
-from tokenizer import tokenize
+from tokenizer import Tokenizer
 import ntpath
 
 
 def main(inputFilePath):
 	zipFile = inputFilePath
 	print("zip file location is: " + zipFile)
-	
+	docID = 1
+	t = Tokenizer()	
 	with zipfile.ZipFile(zipFile, "r") as zipped:
 		for filename in zipped.namelist():
 			if (filename.endswith('.json')):
 				with zipped.open(filename) as f:
 					content = json.loads(f.read())
-					#print(content)
-					tokenize(content)
+					print("tokenizing\n")
+					t.tokenize(content, docID)
+					docID = docID+1
 					time.sleep(300)
 				
 if __name__ == "__main__":
--- tokenizer.py
+++ tokenizer.py
@@ -1,19 +1,27 @@
 import json, time, re, nltk, requests
 from bs4 import BeautifulSoup
 from simhash import Simhash
-nltk.download('punkt')
-
-def tokenize(content):
-	#print(content["content"])
-	chars = set('@.,/:;()-_') #set of chars that dont count as tokens on their own
+#nltk.download('punkt')
+class Tokenizer:
+	def __init__(self):
+		self.inverted = {} #inverted index 
+			
+	def tokenize(self, content, docID):
+		#print(content["content"])
+		chars = set('@.,/:;()-_') #set of chars that dont count as tokens on their own
 	
-	soup = BeautifulSoup(content["content"], 'html.parser')
-	text_tokens = nltk.tokenize.word_tokenize(soup.get_text())
-	for s in text_tokens:
-		if not any((c in chars) for c in s):
-			print(s)
-	time.sleep(300)
-	# TODO: do stuff with text_tokens and write inverse index to content_file
-	#with open("content.txt", 'a', encoding = "utf-8") as content_file:
-	#	print("hi")	
+		soup = BeautifulSoup(content["content"], 'html.parser')
+		text_tokens = nltk.tokenize.word_tokenize(soup.get_text())
+		for s in text_tokens:
+			if not any((c in chars) for c in s): #
+				if s not in self.inverted:
+					self.inverted[s] = [docID]
+				if s in self.inverted:
+					#self.inverted[s].append[docID]
+					print(s + " not in the inverted list\n")
+		print(self.inverted)			
+		time.sleep(300)
+			
+		#with open("content.txt", 'a', encoding = "utf-8") as content_file:
+		#	print("hi")	
 	
