19ecc33847c30d51d2390e2315a84a609343ea78
lolzpokemon <jeffhutton93@gmail.com>
2020-11-13 11:15:21 -0800

    added in tokenized files and new code
    skips files that arent encoded in ascii or utf-8, aslo skips files bigger than 80,000 since it seems to be too big for the tokenzier to handle

--- launch.py
+++ launch.py
@@ -1,4 +1,4 @@
-import zipfile, sys, getopt, time, json, math, os, psutil, signal
+import zipfile, sys, getopt, time, json, math, os, psutil
 from tokenizer import Tokenizer
 from mergesavefiles import Merger
 import ntpath
@@ -7,101 +7,80 @@ import ntpath
 # To look up a word we will only have to load the 4 relevant segment files
 SAVE_TIMES = 20 # number of times to save tokens to disk
 FILE_COUNT = 16 # number of files to segment output into per save
-MAX_TIME_BASE = 10
-MAX_TIME_PER_MB = 10
-
-# timeout signal handler function
-def alarmHandler(sig, frame):
-        raise Exception("Timeout(if you see this, it wasn't caught correctly)")
 
 # which file to write/search for a word in
 def outputFileNum(word):
-	return ord(word[0]) % FILE_COUNT
+    return ord(word[0]) % FILE_COUNT
 
 def main(inputFilePath):
 	# clear the output folder before running
-	for fileName in os.listdir("output/"):
-		print("Removing output/" + fileName)
-		os.remove("output/" + fileName)
+    for fileName in os.listdir("output/"):
+        print("Removing output/" + fileName)
+        os.remove("output/" + fileName)
 
 	# developer.zip
-	zipFile = inputFilePath
-	print("zip file location is: " + zipFile)
+    zipFile = inputFilePath
+    print("zip file location is: " + zipFile)
 	# the document we're on in developer.zip, can look up the specific file any docID refers to via zipped.namelist()[docID]
-	docID = 0
+    docID = 0
 	# the number of documents from developer.zip we've actually tokenized (not the same as docID)
-	filesTokenized = 0
+    filesTokenized = 0
 	# the number of times we've saved the inverted index	
-	saveNum = 0
+    saveNum = 0
 	# set of unique words
-	uniqueWords = set()
+    uniqueWords = set()
 	# tokenizer
-	t = Tokenizer()	
-	# log
-	logFile = open("output/log.txt", "a")
-	# signal handler
-	signal.signal(signal.SIGALRM, alarmHandler)
-	# ctrl-c interrupt cancels the current tokenization
-	signal.signal(signal.SIGINT, alarmHandler)
-	with zipfile.ZipFile(zipFile, "r") as zipped:
-		#Determine which file counts/document IDs to save after
-		inCount = len(zipped.namelist())
-		saveTimes = [math.floor((inCount / SAVE_TIMES) * saveNum) for saveNum in range(1, SAVE_TIMES)] + [inCount - 1]
-		print("{} files, saving at file #: {}".format(inCount, saveTimes))
+    t = Tokenizer()	
+    with zipfile.ZipFile(zipFile, "r") as zipped:
+	#Determine which file counts/document IDs to save after
+        inCount = len(zipped.namelist())
+        saveTimes = [math.floor((inCount / SAVE_TIMES) * saveNum) for saveNum in range(1, SAVE_TIMES)] + [inCount - 1]
+        print("{} files, saving at file #: {}".format(inCount, saveTimes))
 
-		for zinfo in zipped.infolist():
-			if (zinfo.filename.endswith('.json')):
-				with zipped.open(zinfo) as f:
-					# set a timeout on the tokenization
-					fileSizeMB = float(zinfo.file_size) / 1000000
-					fileTimeout = MAX_TIME_BASE + int(MAX_TIME_PER_MB * fileSizeMB)
-					try:
-						signal.alarm(fileTimeout) # timeout
-						logFile.write("tokenizing " + zinfo.filename + " with timeout {}s... ".format(fileTimeout))
-						content = json.loads(f.read())
-						t.tokenize(content, docID)
-						logFile.write("DONE\n")
-					except Exception as exc:
-						logFile.write("TIMEOUT\n")
-						print("WARNING: Timed out on file {}, skipping".format(zinfo.filename))
-					finally:
-						# clear timeout
-						signal.alarm(0)
-					filesTokenized += 1
-			# Save things
-			if docID in saveTimes:
-				print("Beginning save {} of {}".format(saveNum + 1, SAVE_TIMES))
+        for filename in zipped.namelist():
+            fileInfo = zipped.getinfo(filename)
+            if (filename.endswith('.json')):
+                #if file is greater than 80000 kb we skip over it(too big for the tokenizer)
+                if fileInfo.file_size/1000 < 80000:
+                    with zipped.open(filename) as f:
+                        content = json.loads(f.read())
+                        t.tokenize(content, docID)
+                        filesTokenized += 1
+	    # Save things
+            if docID in saveTimes:
+                print("Beginning save {} of {}".format(saveNum + 1, SAVE_TIMES))
 
-				# open output files
-				oFiles = []
-				for i in range(FILE_COUNT):
-					oFile = open("output/output_save{}_block{}.txt".format(saveNum, i), "a")
-					oFiles.append(oFile)
-				#save tokenizer output
-				for word, locs in t.getDict().items():
-					oFiles[outputFileNum(word)].write("{} {}\n".format(word, " ".join([str(loc) for loc in locs])))
-					uniqueWords.add(word)
-				# clear tokenizer, so the next saves don't have our results included
-				t.clearDict()
-				# close output files
-				for oFile in oFiles:
-					oFile.close()
+		# open output files
+                oFiles = []
+                for i in range(FILE_COUNT):
+                    oFile = open("output/output_save{}_block{}.txt".format(saveNum, i), "a", encoding='utf-8')
+                    oFiles.append(oFile)
+		#save tokenizer output
+                for word, locs in t.getDict().items():
+                    oFiles[outputFileNum(word)].write("{} {}\n".format(word, " ".join([str(loc) for loc in locs])))
+                    uniqueWords.add(word)
+		# clear tokenizer, so the next saves don't have our results included
+                t.clearDict()
+		# close output files
+                for oFile in oFiles:
+                    oFile.close()
 
-				print("Completed save {} of {}".format(saveNum + 1, SAVE_TIMES))
+                print("Completed save {} of {}".format(saveNum + 1, SAVE_TIMES))
 
-				saveNum += 1
+                saveNum += 1
 
-			if docID % 200 == 0:
-				process = psutil.Process(os.getpid())
-				print("Files indexed: {:6} Percent done: {:4.2f}% Memory usage: {:8.2f} MB".format(filesTokenized, (docID / inCount) * 100, process.memory_info()[0] / 1000000))
-			docID += 1
+            if docID % 200 == 0:
+                process = psutil.Process(os.getpid())
+                print("Files indexed: {:6} Percent done: {:4.2f}% Memory usage: {:8.2f} MB".format(filesTokenized, (docID / inCount) * 100, process.memory_info()[0] / 1000000))
+            docID += 1
 
 	# find index size on disk in KB
-	sizeKB = 0
-	for fileName in os.listdir("output/"):
-		print("Counting output/" + fileName)
-		sizeKB += os.path.getsize("output/" + fileName) / 1000
+        sizeKB = 0
+        for fileName in os.listdir("output/"):
+                print("Counting output/" + fileName)
+                sizeKB += os.path.getsize("output/" + fileName) / 1000
 
+<<<<<<< Updated upstream
 	# merge save files
 	print("Merging save files")
 	Merger.mergeSaveFiles(SAVE_TIMES, FILE_COUNT)
@@ -110,7 +89,13 @@ def main(inputFilePath):
 	print("Number of files indexed: {}".format(filesTokenized))
 	print("Number of unique words: {}".format(len(uniqueWords)))
 	print("Index disk size in KB: {}".format(sizeKB))
+=======
+        print("========== FINAL REPORT ==========")
+        print("Number of files indexed: {}".format(filesTokenized))
+        print("Number of unique words: {}".format(len(uniqueWords)))
+        print("Index disk size in KB: {}".format(sizeKB))
+>>>>>>> Stashed changes
 
 if __name__ == "__main__":
-	main(sys.argv[1])
+        main(sys.argv[1])
 
--- tokenizer.py
+++ tokenizer.py
@@ -1,35 +1,31 @@
 import json, time, re, nltk, requests
 from bs4 import BeautifulSoup
 from simhash import Simhash
-#nltk.download('punkt')
+
 class Tokenizer:
-	def __init__(self):
-		self.inverted = {} #inverted index 
+    def __init__(self):
+        self.inverted = {} #inverted index 
 			
-	def tokenize(self, content, docID):
-		#print(content["content"])
-		chars = set('@.,/:;()-_') #set of chars that dont count as tokens on their own
-	
-		soup = BeautifulSoup(content["content"], 'html.parser')
-		text_tokens = nltk.tokenize.word_tokenize(soup.get_text())
-		tokens2 = []
-		for item in text_tokens:
-			item = item.lower()
-			if item not in tokens2:
-				tokens2.append(item)
-		for s in tokens2:
-			if not any((c in chars) for c in s): #
-				if s not in self.inverted:
-					self.inverted[s] = [docID]
-				elif (s, docID) not in self.inverted.items(): #TODO: dont append to key if docID value is already posted
-					self.inverted[s].append(docID)
-					#print(s + " not in the inverted list\n")
-		#print(self.inverted)			
-		#time.sleep(300)
-			
-		#with open("content.txt", 'a', encoding = "utf-8") as content_file:
-		#	print("hi")	
-	def getDict(self):
-		return self.inverted
-	def clearDict(self):
-		self.inverted = {}
+    def tokenize(self, content, docID):
+	#print(content["content"])
+        chars = set('@.,/:;()-_') #set of chars that dont count as tokens on their own
+	#only look through files that nltk can read
+        if content["encoding"] == "ascii" or content["encoding"] == "utf-8":
+            soup = BeautifulSoup(content["content"], 'html.parser')
+            text_tokens = nltk.tokenize.word_tokenize(soup.get_text())
+            tokens2 = []
+            for item in text_tokens:
+                item = item.lower()
+                if item not in tokens2:
+                    tokens2.append(item)
+            for s in tokens2:
+                if not any((c in chars) for c in s): 
+                    if s not in self.inverted:
+                        self.inverted[s] = [docID]
+                    elif (s, docID) not in self.inverted.items(): 
+                        self.inverted[s].append(docID)
+					
+    def getDict(self):
+        return self.inverted
+    def clearDict(self):
+        self.inverted = {}
