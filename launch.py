673961fe84c78c3d377b592f735238f19ca78d20
Harrison Clark <harrisonnclark@gmail.com>
2020-11-19 14:17:08 -0800

    fixed space/tab dispute

--- launch.py
+++ launch.py
@@ -16,86 +16,86 @@ FILE_COUNT = 16 # number of files to segment output into per save
 
 # which file to write/search for a word in
 def outputFileNum(word):
-    return ord(word[0]) % FILE_COUNT
+	return ord(word[0]) % FILE_COUNT
 
 def main(inputFilePath):
 	# clear the output folder before running
-    for fileName in os.listdir("output/"):
-        print("Removing output/" + fileName)
-        os.remove("output/" + fileName)
+	for fileName in os.listdir("output/"):
+		print("Removing output/" + fileName)
+		os.remove("output/" + fileName)
 
 	# developer.zip
-    zipFile = inputFilePath
-    print("zip file location is: " + zipFile)
+	zipFile = inputFilePath
+	print("zip file location is: " + zipFile)
 	# the document we're on in developer.zip, can look up the specific file any docID refers to via zipped.namelist()[docID]
-    docID = 0
+	docID = 0
 	# the number of documents from developer.zip we've actually tokenized (not the same as docID)
-    filesTokenized = 0
+	filesTokenized = 0
 	# the number of times we've saved the inverted index	
-    saveNum = 0
+	saveNum = 0
 	# set of unique words
-    uniqueWords = set()
+	uniqueWords = set()
 	# tokenizer
-    t = Tokenizer()	
-    with zipfile.ZipFile(zipFile, "r") as zipped:
+	t = Tokenizer()	
+	with zipfile.ZipFile(zipFile, "r") as zipped:
 	#Determine which file counts/document IDs to save after
-        inCount = len(zipped.namelist())
-        saveTimes = [math.floor((inCount / SAVE_TIMES) * saveNum) for saveNum in range(1, SAVE_TIMES)] + [inCount - 1]
-        print("{} files, saving at file #: {}".format(inCount, saveTimes))
+		inCount = len(zipped.namelist())
+		saveTimes = [math.floor((inCount / SAVE_TIMES) * saveNum) for saveNum in range(1, SAVE_TIMES)] + [inCount - 1]
+		print("{} files, saving at file #: {}".format(inCount, saveTimes))
 
-        for filename in zipped.namelist():
-            fileInfo = zipped.getinfo(filename)
-            if (filename.endswith('.json')):
-                #if file is greater than 80000 kb we skip over it(too big for the tokenizer)
-                if fileInfo.file_size/1000 < 80000:
-                    with zipped.open(filename) as f:
-                        content = json.loads(f.read())
-                        t.tokenize(content, docID)
-                        filesTokenized += 1
-	    # Save things
-            if docID in saveTimes:
-                print("Beginning save {} of {}".format(saveNum + 1, SAVE_TIMES))
+		for filename in zipped.namelist():
+			fileInfo = zipped.getinfo(filename)
+			if (filename.endswith('.json')):
+				#if file is greater than 80000 kb we skip over it(too big for the tokenizer)
+				if fileInfo.file_size/1000 < 80000:
+					with zipped.open(filename) as f:
+						content = json.loads(f.read())
+						t.tokenize(content, docID)
+						filesTokenized += 1
+			# Save things
+			if docID in saveTimes:
+				print("Beginning save {} of {}".format(saveNum + 1, SAVE_TIMES))
 
-		# open output files
-                oFiles = []
-                for i in range(FILE_COUNT):
-                    oFile = open("output/output_save{}_block{}.txt".format(saveNum, i), "a", encoding='utf-8')
-                    oFiles.append(oFile)
-		#save tokenizer output
-                for word, locs in t.getDict().items():
-                    oFiles[outputFileNum(word)].write("{} {}\n".format(word, " ".join([str(loc) for loc in locs])))
-                    uniqueWords.add(word)
-		# clear tokenizer, so the next saves don't have our results included
-                t.clearDict()
-		# close output files
-                for oFile in oFiles:
-                    oFile.close()
+				# open output files
+				oFiles = []
+				for i in range(FILE_COUNT):
+					oFile = open("output/output_save{}_block{}.txt".format(saveNum, i), "a", encoding='utf-8')
+					oFiles.append(oFile)
+				#save tokenizer output
+				for word, locs in t.getDict().items():
+					oFiles[outputFileNum(word)].write("{} {}\n".format(word, " ".join([str(loc) for loc in locs])))
+					uniqueWords.add(word)
+				# clear tokenizer, so the next saves don't have our results included
+				t.clearDict()
+				# close output files
+				for oFile in oFiles:
+					oFile.close()
 
-                print("Completed save {} of {}".format(saveNum + 1, SAVE_TIMES))
+				print("Completed save {} of {}".format(saveNum + 1, SAVE_TIMES))
 
-                saveNum += 1
+				saveNum += 1
 
-            if docID % 200 == 0:
-                process = psutil.Process(os.getpid())
-                print("Files indexed: {:6} Percent done: {:4.2f}% Memory usage: {:8.2f} MB".format(filesTokenized, (docID / inCount) * 100, process.memory_info()[0] / 1000000))
-            docID += 1
-
-	# find index size on disk in KB
-        sizeKB = 0
-        for fileName in os.listdir("output/"):
-                print("Counting output/" + fileName)
-                sizeKB += os.path.getsize("output/" + fileName) / 1000
+			if docID % 200 == 0:
+				process = psutil.Process(os.getpid())
+				print("Files indexed: {:6} Percent done: {:4.2f}% Memory usage: {:8.2f} MB".format(filesTokenized, (docID / inCount) * 100, process.memory_info()[0] / 1000000))
+			docID += 1
 
 	# merge save files
-        print("Merging save files")
-        Merger.mergeSaveFiles(SAVE_TIMES, FILE_COUNT)
+	print("Merging save files")
+	Merger.mergeSaveFiles(SAVE_TIMES, FILE_COUNT)
 
-        print("========== FINAL REPORT ==========")
-        print("Number of files indexed: {}".format(filesTokenized))
-        print("Number of unique words: {}".format(len(uniqueWords)))
-        print("Index disk size in KB: {}".format(sizeKB))
+	# find index size on disk in KB
+	sizeKB = 0
+	for fileName in os.listdir("output/"):
+			print("Counting output/" + fileName)
+			sizeKB += os.path.getsize("output/" + fileName) / 1000
 
-        
+	print("========== FINAL REPORT ==========")
+	print("Number of files indexed: {}".format(filesTokenized))
+	print("Number of unique words: {}".format(len(uniqueWords)))
+	print("Index disk size in KB: {}".format(sizeKB))
+
+		
 if __name__ == "__main__":
-        main(sys.argv[1])
+		main(sys.argv[1])
 
--- tokenizer.py
+++ tokenizer.py
@@ -8,29 +8,29 @@ from bs4 import BeautifulSoup
 from simhash import Simhash
 
 class Tokenizer:
-    def __init__(self):
-        self.inverted = {} #inverted index 
+	def __init__(self):
+		self.inverted = {} #inverted index 
 			
-    def tokenize(self, content, docID):
-	#print(content["content"])
-        chars = set('@.,/:;()-_') #set of chars that dont count as tokens on their own
-	#only look through files that nltk can read
-        if content["encoding"] == "ascii" or content["encoding"] == "utf-8":
-            soup = BeautifulSoup(content["content"], 'html.parser')
-            text_tokens = nltk.tokenize.word_tokenize(soup.get_text())
-            tokens2 = []
-            for item in text_tokens:
-                item = item.lower()
-                if item not in tokens2:
-                    tokens2.append(item)
-            for s in tokens2:
-                if not any((c in chars) for c in s): 
-                    if s not in self.inverted:
-                        self.inverted[s] = [docID]
-                    elif (s, docID) not in self.inverted.items(): 
-                        self.inverted[s].append(docID)
+	def tokenize(self, content, docID):
+		#print(content["content"])
+		chars = set('@.,/:;()-_') #set of chars that dont count as tokens on their own
+		#only look through files that nltk can read
+		if content["encoding"] == "ascii" or content["encoding"] == "utf-8":
+			soup = BeautifulSoup(content["content"], 'html.parser')
+			text_tokens = nltk.tokenize.word_tokenize(soup.get_text())
+			tokens2 = []
+			for item in text_tokens:
+				item = item.lower()
+				if item not in tokens2:
+					tokens2.append(item)
+			for s in tokens2:
+				if not any((c in chars) for c in s): 
+					if s not in self.inverted:
+						self.inverted[s] = [docID]
+					elif (s, docID) not in self.inverted.items(): 
+						self.inverted[s].append(docID)
 					
-    def getDict(self):
-        return self.inverted
-    def clearDict(self):
-        self.inverted = {}
+	def getDict(self):
+		return self.inverted
+	def clearDict(self):
+		self.inverted = {}
